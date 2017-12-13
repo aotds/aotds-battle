3ae2b917dfc6b657c699a772591756760e69fe4f
Yanick Champoux <yanick@babyl.dyndns.org>
2017-12-12 20:03:43 -0500

    remove kiki

--- foo.js
+++
@@ -1,9 +0,0 @@
-
-function bar() {
-    return x => this(x+1)
-}
-
-const foo = function(x){ return x }
-    ::bar();
-
-console.log(foo(4));
--- foo.pl
+++
@@ -1,8 +0,0 @@
-use strict;
-
-my $x = 123;
-
-for my $y (1..5) {
-    potato(;
-    1 + 'a';
-}
