9cde3636692389032ff15c31f74b64a0be66de80
Yanick Champoux <yanick@babyl.dyndns.org>
2018-08-14 23:09:42 -0400

    schema generation for actions

--- tools/generate_schemas.js
+++ tools/generate_schemas.js
@@ -1,10 +1,16 @@
 import fs from 'fs';
-import schema from '../src/schema';
 
-for ( let file of [ '../src/schemas/ship', '../src/schemas/actions' ] ) {
+for ( let file of [ '../src/schemas/ship' ] ) {
     let name = file.replace( '../src/schemas/', 'schemas/'  );
 
     fs.writeFile(name+'.json', JSON.stringify(require(file).default, null, 2), err => {
             if (err) throw err;
     })
 }
+
+
+fs.writeFile('schemas/actions.json', 
+    JSON.stringify(require('../src/actions').default.schema, null, 2), err => {
+        if (err) throw err;
+    }
+)
