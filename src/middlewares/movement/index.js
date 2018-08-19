a5ed1bb8960dac3030daf128ab6532ed0ba777aa
Yanick Champoux <yanick@babyl.dyndns.org>
2018-08-19 16:05:02 -0400

    bogey_movement

--- src/middlewares/movement/index.js
+++ src/middlewares/movement/index.js
@@ -1,20 +1,29 @@
 import _ from 'lodash';
 import fp from 'lodash/fp';
+import u from 'updeep';
 
-import { MOVEMENT_PHASE, bogey_movement } from '~/actions';
+import { MOVEMENT_PHASE, BOGEY_MOVEMENT, actions } from '~/actions';
 import { plot_movement } from '~/movement';
 
-import { get_bogeys, select } from '../selectors';
+import { get_bogey, get_bogeys, select } from '../selectors';
 import { mw_for, subactions } from '../utils';
 
 export const movement_phase = function({getState,dispatch},next,action){
     return getState() 
         |> select( get_bogeys )
         |> fp.filter('navigation')
-        |> fp.map( bogey => bogey_movement(bogey.id) )
+        |> fp.map( bogey => actions.bogey_movement(bogey.id) )
         |> fp.map( dispatch );
 } |> _.curry |> subactions |> mw_for( MOVEMENT_PHASE );
 
+export const bogey_movement = function({getState,dispatch},next,action){
+    let bogey = getState() |> select(get_bogey, action.bogey_id);
+
+    next(u({ navigation: u.constant(plot_movement(bogey)) })(action));
+
+} |> _.curry |> mw_for( BOGEY_MOVEMENT );
+
 export default [
-    movement_phase
+    movement_phase,
+    bogey_movement,
 ];
--- src/middlewares/movement/test.js
+++ src/middlewares/movement/test.js
@@ -1,10 +1,13 @@
 import { actions } from '~/actions';
 
-import { movement_phase } from './index';
+import { movement_phase, bogey_movement } from './index';
 
 const debug = require('debug')('aotds');
 
 import * as selectors from '../selectors';
+
+import * as movement_logic from '~/movement';
+
 
 const mock_mw_args = () => ( {
     store: { getState: jest.fn(), dispatch: jest.fn() },
@@ -39,4 +42,28 @@ test( 'movement_phase', () => {
     expect(mocked.store.dispatch).nthCalledWith(1, actions.bogey_movement('enkidu'));
     expect(mocked.store.dispatch).nthCalledWith(2, actions.bogey_movement('siduri'));
 
+});
+
+test( 'bogey_movement', () => {
+
+    let mocked = mock_mw_args();
+
+    selectors.select = jest.fn( 
+        () => () => ({ id: 'enkidu' })
+    );
+
+    movement_logic.plot_movement = jest.fn( 
+        () => 'worked'
+    );
+
+    bogey_movement(mocked.store,mocked.next,actions.bogey_movement('enkidu'));
+
+    expect(mocked.store.getState).toHaveBeenCalled();
+
+    expect(mocked.store.dispatch).not.toHaveBeenCalled();
+    expect(mocked.next).toHaveBeenCalledWith({
+        type: 'BOGEY_MOVEMENT',
+        bogey_id: 'enkidu',
+        navigation: 'worked',
+    });
 });
