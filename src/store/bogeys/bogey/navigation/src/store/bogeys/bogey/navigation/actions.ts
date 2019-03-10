fbe21e2b2cfe022cc741ad96e9da40e392d7f037
Yanick Champoux <yanick@babyl.ca>
2019-03-10 18:18:27 -0400

    navigation

---
+++ src/store/bogeys/bogey/navigation/src/store/bogeys/bogey/navigation/actions.ts
@@ -0,0 +1,10 @@
+import { NavigationState } from "./types";
+import { action } from "../../../../../../../../../actions";
+
+export const move_bogey = action(
+  'MOVE_BOGEY',
+  (bogey_id: string, navigation: NavigationState) => ({
+    bogey_id,
+    navigation,
+  }),
+);
