cb1b9b0e91084ee8e3ebaf98a927b9fd472398eb
Yanick Champoux <yanick@babyl.ca>
2019-03-10 17:48:33 -0400

    drive bits

---
+++ src/store/bogeys/bogey/drive/reducer.ts
@@ -0,0 +1,29 @@
+// @format
+
+import u from 'updeep';
+import Redactor from '../../../../reducer/redactor';
+
+import { DriveState } from './types';
+import { internal_damage } from '../actions';
+
+const redactor = new Redactor({} as DriveState);
+
+// if damage_level == 1 => half of rating
+// if damage_level == 2 => 0
+const damage_rating = (state: DriveState) => {
+    let { damage_level, rating } = state;
+
+    const current = damage_level === 2 ? 0 : Math.floor(rating / 2);
+
+    return u({ current }, state);
+};
+
+const inc_damage = u({
+    damage_level: (level: number) => Math.min(2, (level || 0) + 1),
+});
+
+redactor.addRedaction(internal_damage, ({ payload: { system: { type } } }) =>
+    u.if(type === 'drive', (drive: DriveState) => damage_rating(inc_damage(drive))),
+);
+
+export const drive_reducer = redactor.asReducer;
---
+++ src/store/bogeys/bogey/drive/types.ts
@@ -0,0 +1,7 @@
+export type DriveState = {
+    rating: number;
+    current: number;
+    damage_level?: 1 | 2;
+};
+
+export type DriveStateShorthand = DriveState | number;
