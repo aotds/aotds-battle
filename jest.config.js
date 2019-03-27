d98e3bfd786c7b8a04ab682ea7b92a94f99e5b38
Yanick Champoux <yanick@babyl.ca>
2019-03-27 12:34:36 -0400

    jest config

--- jest.config.js
+++ jest.config.js
@@ -1,13 +1,13 @@
 // @format
 module.exports = {
-  testEnvironment: 'node',
-  roots: ['./src'],
-  verbose: true,
-  transform: {
-    '^.+\\.(ts|tsx)$': 'ts-jest',
-    '.js$': 'babel-jest',
-  },
-  reporters: ['default', 'jest-html-reporters', 'jest-summary-reporter'],
-  testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.tsx?$',
-  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+    testEnvironment: 'node',
+    roots: ['./src'],
+    verbose: true,
+    transform: {
+        '^.+\\.(ts|tsx)$': 'ts-jest',
+        '.js$': 'babel-jest',
+    },
+    reporters: ['default', 'jest-html-reporters', 'jest-summary-reporter'],
+    testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.tsx?$',
+    moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
 };
