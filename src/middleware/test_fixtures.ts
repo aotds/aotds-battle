db488d0f5353668a9bc5e2aca0c30dbf0bfdc051
Yanick Champoux <yanick@babyl.ca>
2019-04-01 11:27:10 -0400

    adds mw test fixtures

---
+++ src/middleware/test_fixtures.ts
@@ -0,0 +1,26 @@
+import fp from 'lodash/fp';
+import { Middleware } from 'redux';
+
+type MWFixtures = Partial<{
+    dispatch: Function;
+    getState: Function;
+    next: Function;
+    action: object;
+}>;
+
+function mw_fixtures(fixtures: MWFixtures) {
+    return fp.defaults({
+        dispatch: jest.fn(),
+        getState: jest.fn(),
+        next: jest.fn(),
+        action: { type: 'NOOP' },
+    })(fixtures);
+}
+
+export function test_mw(mw: Middleware, fixtures: MWFixtures = {}) {
+    fixtures = mw_fixtures(fixtures);
+
+    mw(fixtures as any)(fixtures.next as any)(fixtures.action);
+
+    return fixtures;
+}
