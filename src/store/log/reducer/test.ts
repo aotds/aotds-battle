cc3ff706fc1e2b6f1ac7cbeff5b3b747005dcfa1
Yanick Champoux <yanick@babyl.dyndns.org>
2019-07-30 20:57:55 -0400

    id => action_id

--- src/store/log/reducer/test.ts
+++ src/store/log/reducer/test.ts
@@ -4,23 +4,23 @@ import { LogState } from './types';
 
 test( 'basic', () => {
     let state : LogState = [
-        { meta: { id: 1 }, type: 'ONE' }
+        { meta: { action_id: 1 }, type: 'ONE' }
     ];
 
     state = log_reducer( state, {
-        type: 'TWO', meta: { id: 2, parent_ids: [ 1 ] },
+        type: 'TWO', meta: { action_id: 2, parent_ids: [ 1 ] },
     })
 
     state = log_reducer( state, {
-        type: 'THREE', meta: { id: 3, parent_ids: [ 1, 2 ] },
+        type: 'THREE', meta: { action_id: 3, parent_ids: [ 1, 2 ] },
     })
 
     state = log_reducer( state, {
-        type: 'FIVE', meta: { id: 5, parent_ids: [ 1,2 ]  },
+        type: 'FIVE', meta: { action_id: 5, parent_ids: [ 1,2 ]  },
     })
 
     state = log_reducer( state, {
-        type: 'FOUR', meta: { id: 4 },
+        type: 'FOUR', meta: { action_id: 4 },
     })
 
     expect(state).toMatchObject([
