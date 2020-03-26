d24ca3f2b52be17e33b0e22ec14fe9163b8ce574
Yanick Champoux <yanick@babyl.dyndns.org>
2020-03-26 13:01:40 -0400

    set orders

--- src/sample_game/turn-1.ts
+++ src/sample_game/turn-1.ts
@@ -4,14 +4,15 @@ import initial_state from './initial_state';
 const {
     init_game,
     set_orders,
+    play_turn,
 } = Battle.actions;
 
 export const actions = [
     init_game(initial_state),
-    set_orders({
-        bogey_id: 'enkidu',
+    set_orders('enkidu',{
         navigation: { thrust: 1, turn: 1, bank: 1 },
     }),
+    play_turn(),
     // try_play_turn(),
     // set_orders('siduri', {
     //     navigation: { thrust: 1 },
@@ -22,4 +23,6 @@ export const actions = [
 export const tests = state => t => {
 
     t.equal( state.game.name, "gemini", 'game name updated' );
+
+    console.log( state.game );
 }
