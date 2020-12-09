262e47d370b40c320a0b9ad27fbecbc6c8ae4e28
Yanick Champoux <yanick@babyl.dyndns.org>
2020-12-09 10:31:55 -0500

    try-play-turn

--- src/dux/bogeys/index.ts
+++ src/dux/bogeys/index.ts
@@ -1,19 +1,27 @@
+import fp from 'lodash/fp';
+
 import Updux from '../../BattleUpdux';
 import { init_game } from '../game/actions';
-import { add_bogey } from './actions';
+import * as actions from './actions';
+import * as selectors from './selectors';
 
 const bogeys_dux = new Updux({
     initial: [],
     actions: {
-        init_game,
-        add_bogey,
+        ...actions,
+        init_game
     },
+    selectors,
 });
 
 bogeys_dux.addSubEffect(init_game, ({ dispatch }) => ({ payload: { bogeys = [] } }) => {
-    bogeys.forEach(bogey => dispatch(add_bogey(bogey)));
+    bogeys.forEach(bogey => dispatch(actions.add_bogey(bogey)));
 });
 
-bogeys_dux.addMutation(add_bogey, (ship => (state: unknown[]) => [...state, ship]) as any);
+bogeys_dux.addEffect(actions.try_play_turn, ({ getState, dispatch, selectors }) => next => action => {
+    if (selectors.readyForNextTurn(getState())) dispatch(actions.play_turn());
+});
+
+bogeys_dux.addMutation(actions.add_bogey, (ship => (state: unknown[]) => [...state, ship]) as any);
 
 export default bogeys_dux.asDux;
