2ca0d63ce0cdbdc4a1e9fde0e5a96f1943a01b85
Yanick Champoux <yanick@babyl.dyndns.org>
2020-12-10 17:32:53 -0500

    bogey_movement

--- src/dux/bogeys/test.ts
+++ src/dux/bogeys/test.ts
@@ -1,6 +1,8 @@
 import dux from '.';
 import { mock_mw } from '../../utils/mock-mw';
 
+const debug = require('debug')('aotds');
+
 test('generates bogey_movement', () => {
     const res = mock_mw(dux.middleware, {
         action: dux.actions.movement_phase(),
@@ -14,4 +16,24 @@ test('generates bogey_movement', () => {
     expect(res.api.dispatch).toHaveBeenCalledTimes(2);
 
     expect(res.api.dispatch).toHaveBeenCalledWith(expect.objectContaining(dux.actions.bogey_movement('a')));
+});
+
+test('movement', () => {
+    const res = mock_mw(dux.middleware, {
+        action: dux.actions.bogey_movement('enkidu'),
+        api: {
+            getState() {
+                return [
+                    {
+                        id: 'enkidu',
+                        navigation: {
+                            velocity: 0,
+                        },
+                    },
+                ];
+            },
+        },
+    });
+
+    expect(res.api.dispatch).toHaveBeenCalled();
 });
