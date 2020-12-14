ca6ab296aa691f750b64ff2b32cb3771d4ffcf6d
Yanick Champoux <yanick@babyl.dyndns.org>
2020-12-14 11:03:25 -0500

    inflate weapons

--- src/dux/bogeys/bogey/weaponry/inflate.test.ts
+++ src/dux/bogeys/bogey/weaponry/inflate.test.ts
@@ -1,12 +1,18 @@
-import {inflate} from '.';
+import { inflate } from '.';
 
-test('inflate', ()=> {
-    const inflated = inflate({
-        firecons: 2,
-    });
-
-    expect(inflated).toMatchObject({
-        firecons: [ {id: 1}, {id: 2} ]
-    })
+const sample_weaponry = inflate({
+    firecons: 2,
+    weapons: [{ weapon_type: 'beam', weapon_level: 1, arcs: ['F'] }],
 });
 
+test('inflate weapons', () => {
+    expect(sample_weaponry).toMatchObject({
+        weapons: [{ id: 1, weapon_type: 'beam', weapon_level: 1, arcs: ['F'] }],
+    });
+});
+
+test('inflate firecons', () => {
+    expect(sample_weaponry).toMatchObject({
+        firecons: [{ id: 1 }, { id: 2 }],
+    });
+});
