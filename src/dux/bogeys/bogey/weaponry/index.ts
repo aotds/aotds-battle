80164171d8fa09f48ca235acf50958e461596f0a
Yanick Champoux <yanick@babyl.ca>
2021-01-07 17:03:52 -0500

    add shields dux and inflate

--- src/dux/bogeys/bogey/weaponry/index.ts
+++ src/dux/bogeys/bogey/weaponry/index.ts
@@ -4,6 +4,7 @@ import u from '@yanick/updeep';
 import firecons, { inflate as inflate_firecons } from './firecons';
 
 import { inflate as inflate_weapons } from './weapons';
+import { inflate_shields } from './shields';
 
 const weaponry_dux = new Updux({
     subduxes: {
@@ -16,4 +17,5 @@ export default weaponry_dux.asDux;
 export const inflate = u({
     firecons: inflate_firecons,
     weapons: inflate_weapons,
+    shields: inflate_shields,
 });
