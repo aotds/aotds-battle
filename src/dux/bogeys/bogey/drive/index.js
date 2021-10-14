d6f611b35821341ebcf3cc32b86bdf8fc52aafe6
Yanick Champoux <yanick@babyl.ca>
2021-10-14 09:40:03 -0400

    drive dux

---
+++ src/dux/bogeys/bogey/drive/index.js
@@ -0,0 +1,29 @@
+import { Updux } from 'updux';
+import fp from 'lodash/fp.js';
+
+// type DriveState = {
+//     rating: number;
+//     current: number;
+//     damage_level?: 0 | 1 | 2;
+// };
+
+export const dux = new Updux({
+    initial: {
+        rating: 0,
+        current: 0,
+    },
+});
+
+//type DriveShorthand = number | DriveState;
+
+export function inflate(shorthand = 0) {
+    if (typeof shorthand === 'number')
+        return {
+            current: shorthand,
+            rating: shorthand,
+            damage_level: 0,
+        };
+
+    return fp.defaults({ damage_level: 0 }, shorthand);
+}
+
