ad2b0171e9a618dd778a4372c5e2b9fdbfc10354
Yanick Champoux <yanick@babyl.ca>
2021-10-20 15:53:59 -0400

    plotMovement

---
+++ src/dux/bogeys/bogey/rules/plotMovement.test.ts
@@ -0,0 +1,241 @@
+import u from 'updeep';
+
+import { plotMovement, moveThrust, moveRotate } from './plotMovement';
+
+type Coords = [number, number];
+
+test('move_thrust', () => {
+	const ship: any = { coords: [0, 0], heading: 1, velocity: 0 };
+
+	const cases: [number, Coords][] = [
+		[0, [0, 0]],
+		[1, [0.5, 0.87]],
+		[10, [5, 8.66]],
+	];
+
+	cases.forEach(([thrust, result]: [number, Coords]) => {
+		expect(moveThrust(ship, thrust)).toMatchObject({
+			coords: result,
+		});
+	});
+});
+
+test('move_rotate', () => {
+	const ship = { coords: [0, 0], heading: 0, velocity: 0 } as any;
+
+	[
+		[0, 0],
+		[1, 1],
+		[-1, 11],
+		[12, 0],
+	].forEach(([turn, heading]) => {
+		expect(moveRotate(ship, turn).heading).toEqual(heading);
+	});
+});
+
+test('simple movements', () => {
+	const angle: { [angle: string]: Coords } = {
+		0: [0, 10],
+		1: [5, 8.66],
+		2: [8.66, 5],
+		3: [10, 0],
+		6: [0, -10],
+		9: [-10, 0],
+		11: [-5, 8.66],
+	};
+
+	const ship = { navigation: { coords: [0, 0], velocity: 10, heading: 0 } };
+
+	for (const a in angle) {
+		ship.navigation.heading = +a;
+		const movement = plotMovement(ship as any);
+		expect(movement).toMatchObject({
+			coords: angle[a],
+			heading: +a,
+		});
+	}
+});
+
+function moveOk(ship, orders, expected) {
+	const navigation = plotMovement(
+		u.updateIn('orders.navigation', orders, ship),
+	);
+
+	expect(navigation).toMatchObject(expected);
+}
+
+test('change of speed', () => {
+	const ship = {
+		navigation: {
+			coords: [0, 0],
+			heading: 0,
+			velocity: 10,
+		},
+		drive: { rating: 6, current: 6 },
+	} as any;
+
+	// accelerate within engine capacity
+	moveOk(ship, { thrust: 6 }, { velocity: 16, coords: [0, 16] });
+
+	// accelerate more than the engine can give
+	moveOk(ship, { thrust: 16 }, { velocity: 16, coords: [0, 16] });
+
+	// decelerate
+	moveOk(ship, { thrust: -6 }, { velocity: 4, coords: [0, 4] });
+
+	// decelerate to min of zero
+	moveOk(
+		u.updateIn('navigation.velocity', 2, ship),
+		{ thrust: -6 },
+		{ velocity: 0, coords: [0, 0] },
+	);
+});
+
+test('turning', () => {
+	const ship = {
+		navigation: { coords: [0, 0], velocity: 5, heading: 0 },
+		drive: { current: 6 },
+	};
+
+	moveOk(
+		ship,
+		{ turn: 3 },
+		{
+			coords: [4, 1.73],
+			velocity: 5,
+			heading: 3,
+		},
+	);
+	moveOk(
+		ship,
+		{ turn: -3 },
+		{
+			coords: [-4, 1.73],
+			velocity: 5,
+			heading: 9,
+		},
+	);
+	moveOk(
+		ship,
+		{ turn: -9 },
+		{
+			coords: [-4, 1.73],
+			velocity: 5,
+			heading: 9,
+		},
+	);
+});
+
+test('banking', () => {
+	const ship = {
+		navigation: {
+			coords: [0, 0],
+			velocity: 5,
+			heading: 0,
+		},
+		drive: { current: 6 },
+	};
+
+	const tests = [
+		[
+			'bank while heading at 3',
+			u({ navigation: { heading: 3 } })(ship),
+			{ bank: -3 },
+			{ coords: [5, 3], heading: 3, velocity: 5 },
+		],
+		[
+			'bank of 3',
+			ship,
+			{ bank: 3 },
+			{ coords: [3, 5], heading: 0, velocity: 5 },
+		],
+		[
+			'bank of -3',
+			ship,
+			{ bank: -3 },
+			{ coords: [-3, 5], heading: 0, velocity: 5 },
+		],
+		[
+			"can't bank more than the limit",
+			ship,
+			{ bank: -9 },
+			{ coords: [-3, 5], heading: 0, velocity: 5 },
+		],
+	];
+
+	tests.forEach(([, ship, orders, expected]) =>
+		moveOk(ship, orders, expected),
+	);
+});
+
+const with_orders = (orders: any) =>
+	(u.updateIn as any)('orders.navigation', orders);
+
+test('complex maneuvers', () => {
+	const ship = {
+		navigation: { coords: [0, 0], velocity: 5, heading: 0 },
+		drive: { current: 6 },
+	};
+
+	const navigation = plotMovement(
+		with_orders({ bank: -1, thrust: -1, turn: 2 })(ship),
+	);
+
+	expect(navigation.trajectory).toEqual([
+		{ type: 'POSITION', coords: [0, 0] },
+		{ type: 'BANK', coords: [-1, 0], delta: [-1, -0] },
+		{ type: 'ROTATE', delta: 1, heading: 1 },
+		{ type: 'MOVE', coords: [0, 1.73], delta: [1, 1.73] },
+		{ type: 'ROTATE', delta: 1, heading: 2 },
+		{ type: 'MOVE', coords: [1.73, 2.73], delta: [1.73, 1] },
+	]);
+});
+
+test('maneuvers', () => {
+	const ship = {
+		navigation: { coords: [0, 0], velocity: 2, heading: 0 },
+		drive: { current: 6 },
+	};
+
+	let course = plotMovement(
+		with_orders({ bank: -1, thrust: -1, turn: 2 })(ship),
+	);
+
+	expect(course.maneuvers).toMatchObject({
+		thrust: [-2, 3],
+		bank: [-3, 3],
+		turn: [-3, 3],
+	});
+
+	course = plotMovement(with_orders({ bank: 0, thrust: -1 })(ship));
+
+	expect(course.maneuvers).toMatchObject({
+		thrust: [-2, 6],
+		bank: [-3, 3],
+		turn: [-3, 3],
+	});
+});
+
+test('course is stable', () => {
+	let ship: any = {
+		navigation: { coords: [0, 0], velocity: 2, heading: 0 },
+		drive: { current: 6 },
+	};
+
+	let course: any = plotMovement(
+		with_orders({ bank: -1, thrust: -1, turn: 2 })(ship),
+	);
+
+	// don't recursively accumulate coursey cruft
+	expect(course).not.toHaveProperty('course');
+
+	ship = u.updateIn('navigation.course', u.constant(course), ship);
+
+	expect(ship).not.toHaveProperty('navigation.course.course');
+
+	course = plotMovement(ship);
+	expect(course).not.toHaveProperty('course');
+
+	ship = u.updateIn('navigation.course', u.constant(course), ship);
+	expect(ship).not.toHaveProperty('navigation.course.course');
+});
---
+++ src/dux/bogeys/bogey/rules/plotMovement.ts
@@ -0,0 +1,184 @@
+import u from 'updeep';
+import fp from 'lodash/fp';
+import _ from 'lodash';
+
+//import bogey from '..';
+
+// import { any } from '../navigation';
+// import { DuxState } from 'updux';
+
+export const round = (n: number) => _.round(n, 2);
+
+//type BogeyState = DuxState<typeof bogey>;
+
+const upush = (new_item: any) => (state = []) => [...state, new_item];
+
+export function moveThrust(navigation: any, thrust: number): any {
+	if (!thrust) return navigation;
+
+	const angle = (navigation.heading * Math.PI) / 6;
+	const delta = [Math.sin(angle), Math.cos(angle)]
+		.map((x) => thrust * x)
+		.map(round);
+
+	const coords = _.zip(navigation.coords, delta).map(_.sum).map(round);
+
+	return u(
+		{
+			trajectory: upush({ type: 'MOVE', delta, coords }),
+			coords,
+		},
+		navigation,
+	);
+}
+
+export function moveBank(movement: any, velocity: number): any {
+	const angle = ((movement.heading + 3) * Math.PI) / 6;
+	const delta = [Math.sin(angle), Math.cos(angle)]
+		.map((x) => velocity * x)
+		.map(round);
+
+	const coords = _.zip(movement.coords, delta).map(_.sum).map(round);
+
+	return u({
+		trajectory: u.withDefault([], upush({ type: 'BANK', delta, coords })),
+		coords,
+	})(movement);
+}
+
+/**
+ * returns a heading between [0,12[
+ */
+function canonicalHeading(heading: number): number {
+	heading %= 12;
+
+	if (heading < 0) heading += 12;
+
+	return heading;
+}
+
+export function moveRotate(movement: any, angle: number): any {
+	const heading = canonicalHeading(movement.heading + angle);
+
+	return u({
+		trajectory: upush({
+			type: 'ROTATE',
+			delta: angle,
+			heading,
+		}),
+		heading,
+	})(movement);
+}
+
+function two_steps(n: number): [number, number] {
+	const split: [number, number] = [_.floor(n / 2), _.ceil(n / 2)];
+
+	if (n < 0) split.reverse();
+
+	return split;
+}
+
+// returns the course of the ship
+export function plotMovement(ship): any {
+	let navigation = fp.omit(['course'], ship.navigation);
+
+	const orders = ship?.orders?.navigation ?? {};
+
+	navigation = u(
+		{ trajectory: [{ type: 'POSITION', coords: navigation.coords }] },
+		navigation,
+	);
+
+	let { thrust = 0, turn = 0, bank = 0 } = orders;
+
+	const engine_rating = ship?.drive?.current ?? 0;
+
+	let engine_power = engine_rating;
+
+	const thrust_range: [number, number] = [
+		Math.max(-engine_power, -navigation.velocity),
+		engine_power,
+	];
+
+	const clamp_thrust = (t: number): number =>
+		_.clamp(t, thrust_range[0], thrust_range[1]);
+
+	if (thrust) {
+		thrust = clamp_thrust(thrust);
+		navigation = u({ velocity: (v: number) => v + (thrust as number) })(
+			navigation,
+		);
+		engine_power -= Math.abs(thrust);
+	}
+
+	if (turn) {
+		const max: number = _.min([
+			_.floor(engine_rating / 2),
+			engine_power,
+		]) as number;
+		turn = _.clamp(turn, -max, max);
+		engine_power -= Math.abs(turn);
+	}
+
+	if (bank) {
+		const max: number = _.min([
+			_.floor(engine_rating / 2),
+			engine_power,
+		]) as number;
+		bank = _.clamp(bank, -max, max);
+		engine_power -= Math.abs(bank);
+
+		navigation = moveBank(navigation, bank);
+	}
+
+	if (turn) {
+		const thr = two_steps(navigation.velocity);
+		const t = two_steps(turn);
+
+		_.zip(t, thr).forEach((m) => {
+			navigation = moveThrust(
+				moveRotate(navigation, m[0] as number),
+				m[1] as number,
+			);
+		});
+	} else {
+		navigation = moveThrust(navigation, navigation.velocity);
+	}
+
+	// navigation = u({ trajectory: upush({
+	//     type: 'POSITION', coords: navigation.coords
+	// })})(navigation);
+
+	const sym_range = (x: number) => [-x, x];
+	const side_maneuver = (current: number): number[] =>
+		sym_range(
+			fp.min([
+				Math.abs(current) + engine_power,
+				_.floor(engine_rating / 2),
+			]) as number,
+		);
+
+	const max_thrust = Math.abs(thrust) + engine_power;
+
+	const maneuvers = {
+		thrust: [fp.max([-max_thrust, -ship.navigation.velocity]), max_thrust],
+		bank: side_maneuver(bank),
+		turn: side_maneuver(turn),
+	};
+
+	return u(
+		{
+			thrust_used: engine_rating - engine_power,
+			maneuvers,
+			coords: u.map(round),
+			orders: {
+				thrust,
+				turn,
+				bank,
+			},
+		} as unknown,
+		navigation,
+	);
+}
+
+export default plotMovement;
