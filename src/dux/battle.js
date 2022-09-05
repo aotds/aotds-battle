a6051afc1d19a6dd63babe84930d8c58b12492c5
Yanick Champoux <yanick@babyl.ca>
2022-09-05 11:49:45 -0400

    add the bogeys dux

--- src/dux/battle.js
+++ src/dux/battle.js
@@ -3,12 +3,14 @@ import { BDux } from '../BDux.js';
 import { dux as actionId, addActionIdEffect } from './actionId.js';
 import { dux as log } from './log.js';
 import { gameDux as game } from './game.js';
+import bogeys from './bogeys';
 
 export const battleDux = new BDux({
 	subduxes: {
 		actionId,
 		log,
 		game,
+        bogeys,
 	},
 });
 
---
+++ src/dux/bogeys.js
@@ -0,0 +1,7 @@
+import { BDux } from '../BDux';
+
+export const bogeysDux = new BDux({
+	initial: [],
+});
+
+export default bogeysDux;
