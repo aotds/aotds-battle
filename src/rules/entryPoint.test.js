2c0297f158a1cb855df18e42dacb66ff1bec32e2
Yanick Champoux <yanick@babyl.ca>
2022-09-08 10:36:08 -0400

    play with the tasks for integration

--- src/rules/entryPoint.test.js
+++ src/rules/entryPoint.test.js
@@ -1,34 +1,33 @@
 import V from '@a-robu/victor';
-import { test, expect,vi } from 'vitest';
+import { test, expect, vi } from 'vitest';
 
 let rollDice;
-vi.mock( '../dice/index.js', () => {
+vi.mock('../dice/index.js', () => {
+	rollDice = vi.fn();
 
-    rollDice = vi.fn();
-
-    return { rollDice };
-} );
+	return { rollDice };
+});
 
 import { entryPoint } from './entryPoint.js';
 
-test( 'basic', () => {
-    rollDice.mockReturnValueOnce([90,50]);
+test('basic', () => {
+	rollDice.mockReturnValueOnce([90, 50]);
 
-    const result = entryPoint();
+	const result = entryPoint();
 
-    expect(result).toHaveLength(2);
+	expect(result).toHaveLength(2);
 
-    expect( V.fromArray(result).length() ).toBeLessThan(50);
-    expect( V.fromArray(result).length() ).toBeGreaterThan(40);
-} );
+	expect(V.fromArray(result).length()).toBeLessThan(50);
+	expect(V.fromArray(result).length()).toBeGreaterThan(40);
+});
 
-test( 'avoid others', () => {
-    rollDice.mockReturnValueOnce([90,50]);
+test('avoid others', () => {
+	rollDice.mockReturnValueOnce([90, 50]);
 
-    rollDice.mockReturnValue([5,5]);
-    const result = entryPoint([[ 35, -25 ]]);
+	rollDice.mockReturnValue([5, 5]);
+	const result = entryPoint([[35, -25]]);
 
-    expect( result ).toEqual([51,-11]);
+	expect(result).toEqual([51, -11]);
 
-    expect( V.fromArray(result).distance(new V(35,-25))).toBeGreaterThan(20);
-} );
+	expect(V.fromArray(result).distance(new V(35, -25))).toBeGreaterThan(20);
+});
