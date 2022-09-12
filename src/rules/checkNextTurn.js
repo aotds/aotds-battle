0c64e4c9404aec10ce50271265e6b5314e065acc
Yanick Champoux <yanick@babyl.ca>
2022-09-12 16:00:05 -0400

    checkNextTurn effect

--- src/rules/checkNextTurn.js
+++ src/rules/checkNextTurn.js
@@ -1,29 +1,25 @@
-import R from 'remeda';
+function checkTurn0(getState, dispatch) {
+	const players = Object.fromEntries(
+		getState().game.players.map(({ name }) => [name, false]),
+	);
 
-function checkTurn0(getState,dispatch) {
-        const players = R.mapValues(
-            getState().game.players, () => false
-        );
+	if (Object.keys(players).length === 0) return;
 
-        if( Object.keys(players).length === 0 ) return;
+	getState()
+		.bogeys.filter(({ orders }) => orders.length > 0)
+		.map(({ player }) => player)
+		.forEach((p) => (players[p] = true));
 
-        getState().bogeys.forEach( ({player}) => {
-            if( player ) players[player] = true;
-        });
-
-        if( Object.values(players).every( x => x ) )
-            dispatch.playNextTurn();
+	if (Object.values(players).every((x) => x)) dispatch.playNextTurn();
 }
 
-export const checkNextTurn = ({getState,dispatch}) => next => action => {
-    next(action);
+export const checkNextTurn = ({ getState, dispatch }) => (next) => (action) => {
+	next(action);
 
-    // if turn 0, we have players *and* ships for each of them
-    if( getState().game.turn === 0 )
-        return checkTurn0(getState,dispatch);
+	// if turn 0, we have players *and* ships for each of them
+	if (getState().game.turn === 0) return checkTurn0(getState, dispatch);
 
-    // for next turns, all active bogeys have orders
-    if( getState.activeBogeys().every(
-        bogey => bogey.orders.length > 0
-    ) ) dispatch.playNextTurn();
-}
+	// for next turns, all active bogeys have orders
+	if (getState.activeBogeys().every((bogey) => bogey.orders.length > 0))
+		dispatch.playNextTurn();
+};
--- src/rules/checkNextTurn.test.js
+++ src/rules/checkNextTurn.test.js
@@ -1,34 +1,63 @@
 import { beforeEach, describe, test, expect, vi } from 'vitest';
+import u from 'updeep';
+
 import { checkNextTurn } from './checkNextTurn';
+
+const fullState = {
+	game: { turn: 0, players: [{ name: 'yanick' }, { name: 'yenzie' }] },
+	bogeys: [
+		{ player: 'yanick', orders: [{ type: 'dummy' }] },
+		{ player: 'yenzie', orders: [{ type: 'dummy' }] },
+	],
+};
 
 const playNextTurn = vi.fn();
 
 const dispatch = {
-    playNextTurn,
+	playNextTurn,
 };
 
 const getState = vi.fn();
 
-const next = () =>{};
+const next = () => {};
 
+describe('turn 0', () => {
+	beforeEach(() => {
+		playNextTurn.mockReset();
+	});
 
-describe( "turn 0", () => {
+	test('no players', () => {
+        getState.mockReturnValue(u.updateIn('game.players', [], fullState ));
 
-    beforeEach( () => {
-        playNextTurn.mockReset()
-    } )
+		checkNextTurn({ getState, dispatch })(next)({});
 
-    test( 'no players', () => {
+		expect(playNextTurn).not.toHaveBeenCalled();
+	});
 
-        getState.mockReturnValue({
-            game: { turn: 0, players: [] }
-        });
+	test('players, but no orders', () => {
+        getState.mockReturnValue(
+            u({bogeys: u.map(
+            { orders: []}
+        )}, fullState));
 
-        checkNextTurn({getState, dispatch })(next)({});
+		checkNextTurn({ getState, dispatch })(next)({});
 
-        // no players? No turn
-        expect(dispatch.playNextTurn).not.toHaveBeenCalled();
-    });
+		expect(playNextTurn).not.toHaveBeenCalled();
+	});
 
+	test('players, but no orders for all ships', () => {
+		getState.mockReturnValue(u.updateIn('bogeys.1.orders', [], fullState));
+
+		checkNextTurn({ getState, dispatch })(next)({});
+
+		expect(playNextTurn).not.toHaveBeenCalled();
+	});
+
+	test('players and orders for all ships', () => {
+		getState.mockReturnValue(fullState);
+
+		checkNextTurn({ getState, dispatch })(next)({});
+
+		expect(dispatch.playNextTurn).toHaveBeenCalled();
+	});
 });
-
